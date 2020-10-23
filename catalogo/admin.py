03c1f92d532de547c5c6f93ae9fb14ef209cf274
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2020-10-22 21:26:14 -0300

    App Catalogo con errores de modelo

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Autor, Genero, Libro, Idioma, Inventario
 
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Genero)
+admin.site.register(Libro)
+admin.site.register(Idioma)
+admin.site.register(Inventario)
