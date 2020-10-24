2135e75fa819394fc9fd179943389ceb94eb4e2b
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2020-10-24 13:16:17 -0300

    Modificación backend catalogo

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import Autor, Genero, Libro, Idioma, Inventario
 
 # Register your models here.
-admin.site.register(Autor)
-admin.site.register(Genero)
-admin.site.register(Libro)
 admin.site.register(Idioma)
-admin.site.register(Inventario)
+admin.site.register(Genero)
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'fecha_nacimiento', 'fecha_muerte')
+    fields = [('nombre', 'apellido'), 'fecha_nacimiento', 'fecha_muerte']
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'isbn')
+
+@admin.register(Inventario)
+class InventarioAdmin(admin.ModelAdmin):
+    list_filter = ('estatus', 'devolucion')
+    fieldsets = (
+        ('Libro', {
+            'fields': ('libro', 'impreso', 'idioma', 'id')
+        }),
+        ('Disponibilidad', {
+            'fields': ('estatus', 'devolucion')
+        })
+    )
+
