5e7bc18fc405a313a7fe3946dcb39ca450c9b818
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2020-10-24 14:04:04 -0300

    Desarrollo de index y base generica

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,6 +1,7 @@
 from django.urls.conf import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    
+    re_path(r'^$', views.index, name="index") # función url -> re_path
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,14 @@
+from django.contrib.admin import autodiscover
 from django.shortcuts import render
+from .models import Libro, Inventario, Autor
 
 # Create your views here.
+def index(request):
+    numero_libros = Libro.objects.all().count()
+    numero_inventario = Inventario.objects.all().count()
+    numero_inventario_disponible = Inventario.objects.filter(estatus__exact='d').count()
+    numero_autores = Autor.objects.count() # El all() esta implicido por defecto.
+
+    # Renderiza la plantilla HTML index.html con los en la variable contexto
+    return render(request, 'index.html', context={'numero_libros': numero_libros, 'numero_inventario': numero_inventario, 
+                                                  'numero_inventario_disponible': numero_inventario_disponible, 'numero_autores': numero_autores})
