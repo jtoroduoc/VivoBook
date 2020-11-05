dd689b81ceb245491227dbcb213dd9d05c508c3b
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2020-11-05 20:51:55 -0300

    Nuevo cambio

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mtopqx*!=ckyafs28u%=5sr6lx8fzj6=+#93gx3i*6*wb*$1mx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
